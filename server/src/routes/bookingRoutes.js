3c201b4f48fa6f85e0ee3362f2bc85a76abbf624
ShreyaVerm-a <145741114+ShreyaVerm-a@users.noreply.github.com>
2026-06-18 21:37:46 +0530

    Delete server directory

--- server/src/routes/bookingRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/bookingController");
-
-router.get("/", controller.getBookings);
-router.post("/", controller.createBooking);
-
-module.exports = router;
--- server/src/routes/roomRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/roomController");
-
-router.get("/", controller.getRooms);
-router.post("/", controller.createRoom);
-
-module.exports = router;
